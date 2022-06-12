01812abe110ca23cdab911bc8474c027c13046bd
Leytek <leytek@yandex.ru>
2022-06-12 13:35:30 +0300

    fix: throw error in cd when path is file

--- src/operations/nwd/cd.js
+++ src/operations/nwd/cd.js
@@ -1,9 +1,14 @@
-import {access} from 'fs/promises';
+import {access, stat} from 'fs/promises';
 import {resolve} from 'path';
 import globalVar from '../../globalVar.js';
 
 export default async function cd(targetPath) {
-  let newCWD = resolve(globalVar.currentWorkingDir, targetPath);
-  await access(newCWD);
-  globalVar.currentWorkingDir = newCWD;
+  let path = resolve(globalVar.currentWorkingDir, targetPath);
+  let pathStat = await stat(path);
+
+  if(pathStat.isFile())
+    throw new Error('Cannot change current directory: given path is file.');
+
+  await access(path);
+  globalVar.currentWorkingDir = path;
 }
