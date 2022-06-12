76c93f0f6ab8747ac98a5e18b095fcbceafd4b6f
Leytek <leytek@yandex.ru>
2022-06-12 12:56:30 +0300

    refactor: eol

--- src/operations/os/eol.js
+++ src/operations/os/eol.js
@@ -1,8 +1,5 @@
 import os from 'os';
 
 export default function eol() {
-  let eol = os.EOL === '\n' ? '\\n' :
-            os.EOL === '\r\n' ? '\\r\\n' :
-            "unknown";
-  return eol;
+  return JSON.stringify(os.EOL);
 }
